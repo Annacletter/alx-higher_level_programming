7e8965411d0e5ab856f72b883d563b5bcddea3c4
Annacletter <anna.cletter12@gmail.com>
2023-08-08 11:04:47 -0700

    Task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+final  = abs(number) % 10
+sign = -1 if number < 10 else 1
+final *= sign
+
+comparison = "greater than 5" if final > 5 else "less than 6 and not 0" if final != 0 else "0"
+
+print(f"Last digit of {number} is {final} and is {comparison}")
