b628bbeac19c3615d19aa7327d133ceeb8b9843f
Annacletter <anna.cletter12@gmail.com>
2023-08-08 11:29:31 -0700

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,6 @@ final = abs(number) % 10
 sign = -1 if number < 10 else 1
 final *= sign
 
-diff = "greater than 5" if final > 5 else "less than 6 and not 0" if final != 0 else "0"
+diff = "greater than 5"
+if final > 5 else "less than 6 and not 0" if final != 0 else "0"
 print(f"Last digit of {number} is {final} and is {diff}")
