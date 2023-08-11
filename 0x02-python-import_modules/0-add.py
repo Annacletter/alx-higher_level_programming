87bdf61a92962a5b6e512957bb8763098b3a6548
Annacletter <anna.cletter12@gmail.com>
2023-08-11 09:25:40 -0700

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,11 @@
 
 if __name__ == "__main__":
 
-from add_0 import add
+    from add_0 import add
 
 a = 1
 b = 2
 
 result = add(a, b)
 
-print("a{} + b{} = {}".format(a, b, add))
+print("a{} + b{} = {}".format(a, b, add)))
