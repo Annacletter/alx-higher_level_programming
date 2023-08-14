ced544045b8417a025c1888696ade21d419e94e4
Annacletter <anna.cletter12@gmail.com>
2023-08-14 11:18:11 -0700

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def max_integer(my_list=None):
-    if my_list is None or not my_list:
+
+def max_integer(my_list=[]):
+    if not my_list:
         return None
-    else:
-        biggest = max(my_list)
-        return biggest
+    biggest = max(my_list)
+    return biggest
