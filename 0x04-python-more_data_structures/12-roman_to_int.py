866892fa3274286a44cffceb9764d9a17372c85b
Annacletter <anna.cletter12@gmail.com>
2023-08-16 13:49:54 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                        'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    roman = roman_string
-    
-    if not isinstance(roman, str) or len(roman) == 0:
-        return 0
-    
-    for index in range(len(roman)):
-        if index < len(roman) - 1 and roman_dictionary[roman[index]] < roman_dictionary[roman[index + 1]]:
-            result -= roman_dictionary[roman[index]]
-        else:
-            result += roman_dictionary[roman[index]]
-    
-    return result
-This version of the code should work correctly for converting Roman numerals to integers.
-
-
-
-
-
-Regenerate#!/usr/bin/python3
-
-def roman_to_int(roman_string):
-    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                        'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    roman = roman_string
-    
-    if not isinstance(roman, str) or len(roman) == 0:
-        return 0
-    
-    for index in range(len(roman)):
-        if index < len(roman) - 1 and roman_dictionary[roman[index]] < roman_dictionary[roman[index + 1]]:
-            result -= roman_dictionary[roman[index]]
-        else:
-            result += roman_dictionary[roman[index]]
-    
-    return result
+        roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                            'C': 100, 'D': 500, 'M': 1000}
+        answer = 0
+        component = 0
+        roman = roman_string
+        if type(roman) is not str or len(roman) is 0:
+                return 0
+        for component in range(component, len(roman)):
+                if component < len(roman) - 1\
+                   and\
+                   roman_dictionary[roman[component]] <\
+                   roman_dictionary[roman[component + 1]]:
+                    answer -= roman_dictionary[roman[component]]
+                else:
+                    answer += roman_dictionary[roman[component]]
+        return answer
