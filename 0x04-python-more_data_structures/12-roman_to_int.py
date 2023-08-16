665a627e8bb0527ec6d74d8b4f6778efd4f7ac33
Annacletter <anna.cletter12@gmail.com>
2023-08-16 14:00:39 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-roman_digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                'M': 1000, '0': 0}
-
-
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
+    if type(roman_string) is not str:
         return 0
-    roman_string += '0'
-    i, sum = 0, 0
-    while i < len(roman_string) - 1:
-        current = roman_digits[roman_string[i]]
-        next = roman_digits[roman_string[i + 1]]
-        if current == next:
-            sum += next + curent
-            i += 1
-        elif this < next:
-            sum += next - current
-            i += 1
-        else:
-            sum += current
-        i += 1
-    return sum
+    if roman_string is None:
+        return 0
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    leng = len(roman_string) - 1
+    if roman_string[leng] not in roman:
+        return 0
+    number = roman[roman_string[leng]]
+    for i in range(leng, 0, -1):
+        if roman_string[i] not in roman:
+            return 0
+        now = roman[roman_string[i]]
+        if roman_string[i - 1] not in roman:
+            return 0
+        prev = roman[roman_string[i - 1]]
+        if i > 1:
+            if roman_string[i - 2] not in roman:
+                return 0
+            if prev < now and roman[roman_string[i - 2]] <= prev:
+                return 0
+        if i > 2:
+            if prev == now and roman[roman_string[i - 2]] == prev:
+                if roman_string[i - 3] not in roman:
+                    return 0
+                if roman[roman_string[i - 3]] == roman[roman_string[i - 2]]:
+                    return 0
+        number += prev if prev >= now else - prev
+    return number
