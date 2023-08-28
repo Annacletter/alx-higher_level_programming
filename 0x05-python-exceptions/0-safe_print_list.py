f4f6394f2803133c62f78458ab8da6e277e7d1a4
Annacletter <anna.cletter12@gmail.com>
2023-08-28 12:04:27 -0700

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=None, x=0):
     if my_list is None:
         my_list = []
-        
+
     count = 0
     for i in range(x):
         try:
@@ -10,5 +11,6 @@ def safe_print_list(my_list=None, x=0):
             count += 1
         except IndexError:
             break
+
     print()
     return count
