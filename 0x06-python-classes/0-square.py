b97635a6f9f5ad44d380cf8856e5b4c2d89f4e94
Annacletter <anna.cletter12@gmail.com>
2023-08-29 12:10:22 -0700

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, side_length):
-        self.side_length = side_length
-
-    def area(self):
-        return self.side_length ** 2
-
-    def perimeter(self):
-        return 4 * self.side_length
+    """ This is an empty class Square. """
+    pass
