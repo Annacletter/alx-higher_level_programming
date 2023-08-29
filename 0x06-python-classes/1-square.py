7f906917a2d283f6a5d680a48030fc7d3399d224
Annacletter <anna.cletter12@gmail.com>
2023-08-29 12:36:19 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This defines a class square."""
+class Square:
+    """Shows a square."""
+
+    def __init__(self, size):
+        """Initializes the size attribute."""
+        self.__size = size
