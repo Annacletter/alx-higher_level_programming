b7b63d550599a084b31b6618d74b84b34e94feff
Annacletter <anna.cletter12@gmail.com>
2023-08-29 12:46:31 -0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size=0):
+        """Initialize a Square instance.        
+        Args:
+            size (int): The size of the square.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
