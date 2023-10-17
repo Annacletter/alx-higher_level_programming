36adb355d7f1ec22e3ef9174c144dd1c0742a3d8
Annacletter <anna.cletter12@gmail.com>
2023-10-17 11:42:33 -0700

    SQL

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---creates the database hbtn_0c_0 in my MySQL server 
+-- creates the database hbtn_0c_0 in my MySQL server 
  -- If it already exists, script should fail 
  CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
