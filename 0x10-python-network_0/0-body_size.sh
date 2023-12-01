71f1a18f839d55eb700d47489fcea14b375c4c72
Annacletter <anna.cletter12@gmail.com>
2023-12-01 11:11:46 -0800

    Networking 0

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+#takes in a URL, request to that URL, and displays body size
+curl -sI "$1" | grep Content-Length | cut -d ' ' -f 2
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#URL, GET request to that URL, and displays the response body
+curl -Ls "$1"
